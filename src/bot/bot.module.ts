d9cb9af64e11971f734f1c9ea3b2a3001f73411c
RaffDevs <rveronezdeveloper@gmail.com>
2021-07-24 22:17:11 -0300

    implementação da feature de mensagens

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MessagesModule } from 'src/messages/messages.module';
 import { BotController } from './controllers/bot.controller';
 import { CreateSessionService } from './services/create-session.service';
+import { MessageFactory } from './services/message-factory.service';
 import { MessageHandlerService } from './services/message-handler.service';
 
 
 @Module({
+  imports: [MessagesModule],
   providers: [
     CreateSessionService, 
-    MessageHandlerService
+    MessageHandlerService,
+    MessageFactory,
   ],
   controllers: [BotController]
 })
